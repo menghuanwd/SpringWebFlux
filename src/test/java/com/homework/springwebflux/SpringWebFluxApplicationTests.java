2e3ede50c18c367521eb47f4dae6124d382f3b72
menghuanwd <651019063@qq.com>
2022-11-30 00:25:29 +0800

    init project

---
+++ src/test/java/com/homework/springwebflux/SpringWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
